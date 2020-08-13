78c78172dc1d82ab6bb0413206b1389e95274ec0
nefejames <nefejames@yahoo.com>
2020-08-13 14:26:15 -0700

    added react-router setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage/Homepage";
 import "./App.css";
 
+const hatspage = () => <p>hats page</p>;
+const menspage = () => <p>mens page</p>;
+
 function App() {
-  return <Homepage />;
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/hats" component={hatspage} />
+        <Route exact path="/men" component={menspage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
